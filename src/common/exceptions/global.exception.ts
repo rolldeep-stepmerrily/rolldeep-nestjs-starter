d9f6f2d0b6e066fe25e38b69d137fd394c1c7a17
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2024-11-06 10:22:57 +0900

    update : add global exception

--- src/common/exceptions/global.exception.ts
+++ src/common/exceptions/global.exception.ts
@@ -11,4 +11,9 @@ export const GLOBAL_ERRORS = {
     errorCode: 'INVALID_POSITIVE_INT',
     message: 'Invalid positive integer',
   },
+  UNKNOWN_ERROR: {
+    statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+    errorCode: 'UNKNOWN_ERROR',
+    message: '알 수 없는 에러가 발생하였습니다.',
+  },
 };
